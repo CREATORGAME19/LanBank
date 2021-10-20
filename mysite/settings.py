4c81a758025a3ae911bf22ee0c7f0df1b6edb649
Calin Novogreblevschi <46006265+CREATORGAME19@users.noreply.github.com>
2021-10-20 12:41:22 +0100

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = '!di#jgeji848@l378ge)dd9^kvqjb*a(6-=p78=d!24mshj+4%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'carmennovo19@gmail.com'
+EMAIL_HOST_PASSWORD = 'Minecraft19'
 
 # Application definition
 
